b70357b4b81d5bb294f31aac75acc7c830b7fcca
quadrman <kykyshkaigroman@gmail.com>
2023-10-17 15:39:46 +0900

    New files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Сумма_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a= int.Parse(Console.ReadLine());
+            int tempn = a / 10;
+            int tempn2 = a-tempn * 10;
+            a = tempn + tempn2;
+            Console.WriteLine("Сумма числа равна:" + a) ;
+            Console.ReadKey();
+        }
+    }
+}
